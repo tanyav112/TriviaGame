a2f15166fea7eeba38b992a89f891195ad162e44
Tanya Valdez <TValdez@digmap.com>
2018-08-27 23:09:18 -0700

    add game logic, add to css, add header

--- javascript/app.js
+++ javascript/app.js
@@ -1,40 +1,48 @@
 let questions = [
-   {  
+   {
+     id: 0,
      question: 'The average person does what thirteen times a day?',
-     choices: ['Cries', 'Laughs', 'Farts', "Pee's", 'Scratch themselves'],
+     choices: ['Cries', 'Laughs', 'Farts', 'Sneezes', 'Scratch themselves'],
      correct: 'Laughs'
    },
   {
-    question: "In Texas it's illegal to swear in front of what?",
-    choices: ['A child', 'The governor', 'A corpse', 'The pope', 'A judge'],
-    correct: 'A corpse'
+   id: 1,
+   question: "In Texas it's illegal to swear in front of what?",
+   choices: ['A child', 'The governor', 'A corpse', 'The pope', 'A judge'],
+   correct: 'A corpse'
   },
   {
-    question: 'How many colors are there in a rainbow? ',
-    choices: ['5', '10', '7', '9', '6'],
-    correct: '7 '
+   id: 2,
+   question: 'How many colors are there in a rainbow? ',
+   choices: ['5', '10', '7', '9', '6'],
+   correct: '7 '
   },
   {
-    question: 'How many squares are there on a chess board? ',
-    choices: ['32', '48', '56', '64', '72'],
-    correct: '64 '
+   id: 3,
+   question: 'How many squares are there on a chess board? ',
+   choices: ['32', '48', '56', '64', '72'],
+   correct: '64 '
   },
   {
-    question: "According to Green mythology, who stole fire for mankind's benefit?",
-    choices: ['Zeus', 'Poseido', 'Artemis', 'Apollo', 'Prometheus' ],
-    correct: 'Prometheus '
+    id: 4,
+   question: "According to Green mythology, who stole fire for mankind's benefit?",
+   choices: ['Zeus', 'Poseido', 'Artemis', 'Apollo', 'Prometheus' ],
+   correct: 'Prometheus '
   },
   {
-  question: 'Who were the first television couple to be shown in bed together on prime time television? ',
-    choices: ['Fred and Wilma Flintstone', 'Lucy and Ricky Ricardo', 'Archie and Edith Bunker', 'Herman and Lily Munster', 'Mike and Carol Brady'],
-    correct: 'Fred and Wilma Flintstone '
+   id: 5,
+   question: 'Who were the first television couple to be shown in bed together on prime time television? ',
+   choices: ['Fred and Wilma Flintstone', 'Lucy and Ricky Ricardo', 'Archie and Edith Bunker', 'Herman and Lily Munster', 'Mike and Carol Brady'],
+   correct: 'Fred and Wilma Flintstone '
   },
   {
+    id: 6,
     question: 'Which country was the Caesar salad invented in? ',
     choices: ['United States', 'Italy', 'Mexico', 'Spain', 'Greece'],
     correct: 'Mexico '
   },
   {
+    id: 7,
     question: "What is the world's most popular first name?",
     choices: ['John', 'George', 'Jacob', 'Jesus', 'Muhammad'],
     correct: 'Muhammad '
@@ -42,57 +50,55 @@ let questions = [
 ]
 
 questions.forEach(question => {
-$('.container').append(`
+$('.trivia').append(`
   <div class="row">
     <h5 class="question">${question.question}</h5>
          <p>
           <label>
-              <input name="group1" type="radio"  />
-              <span>${question.choices[0]}</span>
+              <input class="choice" data-choice="${question.choices[0]}" name="question-${question.id}" type="radio"  />
+              <span class="answer">${question.choices[0]}</span>
             </label>
           </p>
           <p>
             <label>
-               <input name="group1" type="radio" />
-               <span>${question.choices[1]}</span>
+               <input class="choice" data-choice="${question.choices[1]}" name="question-${question.id}" type="radio" />
+               <span class="answer">${question.choices[1]}</span>
              </label>
            </p>
           <p>
             <label>
-              <input name="group1" type="radio" />
-              <span>${question.choices[2]}</span>
+              <input class="choice" data-choice="${question.choices[2]}" name="question-${question.id}" type="radio" />
+              <span class="answer">${question.choices[2]}</span>
             </label>
           </p>
           <p>
             <label>
-              <input name="group1" type="radio" />
-              <span>${question.choices[3]}</span>
+              <input class="choice" data-choice="${question.choices[3]}" name="question-${question.id}" type="radio" />
+              <span class="answer">${question.choices[3]}</span>
             </label>
           </p>
           <p>
             <label>
-              <input name="group1" type="radio" />
-              <span>${question.choices[4]}</span>
+              <input class="choice" data-choice="${question.choices[4]}" name="question-${question.id}" type="radio" />
+              <span class="answer">${question.choices[4]}</span>
             </label>
           </p>
           <p>
-            <label>
-              <input name="group1" type="radio" />
-              <span>${question.choices[5]}</span>
-            </label>
-          </p>
-         <p>
-           <label>
-              <input name="group1" type="radio" />
-              <span>${question.choices[6]}</span>
-            </label>
-          </p>
-         <p class="answer">${question.correct}</p>
+          <p class="answer">${question.correct}</p>
+         <br>
+          <br>
     </div>
       `)
     })
      
+//click event for radio buttons
+$(document).on('click', '.choice', function() {
+console.log($(this).attr('name'))
+console.log($(this).attr('data-choice'))
 
+
+
+})
 
 
 
